9e9ea64b3bce62a60fb65f4b062a0785c475dbf5
Jakub Knap <jknap40@gmail.com>
2023-03-11 14:00:09 +0100

    Dodawanie nowego produktu

--- src/app/layouts/fullpageadmin/fullpageadmin.module.ts
+++ src/app/layouts/fullpageadmin/fullpageadmin.module.ts
@@ -8,13 +8,17 @@ import { MaterialModule } from '../../shared/material.module';
 import { AdminProductComponent } from '../../modules/admin/admin-product/admin-product.component';
 import { AdminProductUpdateComponent } from '../../modules/admin/admin-product-update/admin-product-update.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AdminProductAddComponent } from 'src/app/modules/admin/admin-product-add/admin-product-add.component';
+import { AdminProducFormComponent } from '../../modules/admin/admin-product-form/admin-product-form.component';
 
 @NgModule({
   declarations: [
     FullpageadminComponent,
     AdminComponent,
     AdminProductComponent,
-    AdminProductUpdateComponent
+    AdminProductUpdateComponent,
+    AdminProductAddComponent,
+    AdminProducFormComponent
   ],
   imports: [
     CommonModule,
