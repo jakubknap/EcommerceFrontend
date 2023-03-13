a7a1d7e8d30562125b39cb50b3416a76086d75d1
Jakub Knap <jknap40@gmail.com>
2023-03-13 12:24:51 +0100

    Pełny opis produktu

--- src/app/modules/admin/admin-product-update/admin-product-update.component.ts
+++ src/app/modules/admin/admin-product-update/admin-product-update.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AdminProductUpdateService } from './admin-product-update.service';
-import { AdminProductUpdate } from './model/adminProductUpdate';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 import { AdminMessageService } from '../admin-message.service';
+import { AdminProductUpdateService } from './admin-product-update.service';
+import { AdminProductUpdate } from './model/adminProductUpdate';
 
 @Component({
   selector: 'app-admin-product-update',
   templateUrl: './admin-product-update.component.html',
-  styleUrls: ['./admin-product-update.component.scss'],
+  styleUrls: ['./admin-product-update.component.scss']
 })
 export class AdminProductUpdateComponent implements OnInit {
+
   product!: AdminProductUpdate;
   productForm!: FormGroup;
   requiredFileTypes = "image/jpeg, image/png";
@@ -24,57 +25,57 @@ export class AdminProductUpdateComponent implements OnInit {
     private formBuilder: FormBuilder,
     private snackBar: MatSnackBar,
     private adminMessageService: AdminMessageService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.getProduct();
 
     this.productForm = this.formBuilder.group({
-      name: ['',[Validators.required, Validators.minLength(4)]],
-      description: ['',[Validators.required, Validators.minLength(4)]],
-      category: ['',[Validators.required, Validators.minLength(4)]],
-      price: ['',[Validators.required, Validators.min(0)]],
-      currency: ['PLN',Validators.required],
-      slug: ['',[Validators.required, Validators.minLength(4)]],
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      description: ['', [Validators.required, Validators.minLength(4)]],
+      fullDescription: [''],
+      category: ['', [Validators.required, Validators.minLength(4)]],
+      price: ['', [Validators.required, Validators.min(0)]],
+      currency: ['PLN', Validators.required],
+      slug: ['', [Validators.required, Validators.minLength(4)]]
     });
 
     this.imageForm = this.formBuilder.group({
       file: ['']
-    });
+    })
   }
 
   getProduct() {
     let id = Number(this.router.snapshot.params['id']);
-    this.adminProductUpdateService
-      .getProduct(id)
-      .subscribe((product) => this.mapFormValues(product));
+    this.adminProductUpdateService.getProduct(id)
+      .subscribe(product => this.mapFormValues(product));
   }
 
   submit() {
     let id = Number(this.router.snapshot.params['id']);
-    this.adminProductUpdateService
-      .saveProduct(id, {
-        name: this.productForm.get('name')?.value,
-        description: this.productForm.get('description')?.value,
-        category: this.productForm.get('category')?.value,
-        price: this.productForm.get('price')?.value,
-        currency: this.productForm.get('currency')?.value,
-        slug: this.productForm.get('slug')?.value,
-        image: this.image
-      } as AdminProductUpdate).subscribe({
-        next: (product) => {
-          this.mapFormValues(product);
-          this.snackBar.open('Produkt został zapisany', '', { duration: 3000 });
-        },
-        error: (err) => this.adminMessageService.addSpringErrors(err.error),
-      });
+    this.adminProductUpdateService.saveProduct(id, {
+      name: this.productForm.get('name')?.value,
+      description: this.productForm.get('description')?.value,
+      fullDescription: this.productForm.get('fullDescription')?.value,
+      category: this.productForm.get('category')?.value,
+      price: this.productForm.get('price')?.value,
+      currency: this.productForm.get('currency')?.value,
+      slug: this.productForm.get('slug')?.value,
+      image: this.image
+    } as AdminProductUpdate).subscribe({
+      next: product => {
+        this.mapFormValues(product);
+        this.snackBar.open("Produckt został zapisany", '', { duration: 3000 });
+      },
+      error: err => this.adminMessageService.addSpringErrors(err.error)
+    });
   }
 
   uploadFile(){
-      let formData = new FormData();
-      formData.append('file', this.imageForm.get('file')?.value);
-      this.adminProductUpdateService.uploadImage(formData).subscribe(
-        result => this.image = result.filename);
+    let formData = new FormData();
+    formData.append('file', this.imageForm.get('file')?.value);
+    this.adminProductUpdateService.uploadImage(formData)
+      .subscribe(result => this.image = result.filename);
   }
 
   onFileChange(event: any){
@@ -89,6 +90,7 @@ export class AdminProductUpdateComponent implements OnInit {
     this.productForm.setValue({
       name: product.name,
       description: product.description,
+      fullDescription: product.fullDescription,
       category: product.category,
       price: product.price,
       currency: product.currency,
